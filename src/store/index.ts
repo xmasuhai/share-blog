8680f8cf8beca77ab1ba32fcfcca6a8ab57fe69c
xmasuhai <xmasuhai@163.com>
2022-02-05 01:19:08 +0800

    ⬆️ :sparkles: vue@3.2.29

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,9 @@
-import { createStore } from 'vuex'
+// try pinia
+import {createStore} from 'vuex';
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+});
